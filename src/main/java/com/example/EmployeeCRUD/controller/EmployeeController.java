66caf0169380feebdb2886d9019bdea749f8f8e4
A Alyque Hazin <alyquekj@gmail.com>
2021-06-15 10:07:01 +0530

    uploading files

---
+++ src/main/java/com/example/EmployeeCRUD/controller/EmployeeController.java
@@ -0,0 +1,58 @@
+package com.example.EmployeeCRUD.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.EmployeeCRUD.domain.Employee;
+import com.example.EmployeeCRUD.service.EmployeeService;
+
+@Controller
+public class EmployeeController {
+	@Autowired
+    private EmployeeService service;
+
+    @GetMapping("/")
+    public String viewHomePage(Model model) {
+        List<Employee> listemployee = service.listAll();
+        model.addAttribute("listemployee", listemployee);
+        System.out.print("Get / ");	
+        return "index";
+    }
+
+    @GetMapping("/new")
+    public String add(Model model) {
+        model.addAttribute("employee", new Employee());
+        return "new";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveStudent(@ModelAttribute("employee") Employee std) {
+        service.save(std);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/edit/{id}")
+    public ModelAndView showEditStudentPage(@PathVariable(name = "id") int id) {
+        ModelAndView mav = new ModelAndView("new");
+        Employee std = service.get(id);
+        mav.addObject("employee", std);
+        return mav;
+        
+    }
+    @RequestMapping("/delete/{id}")
+    public String deletestudent(@PathVariable(name = "id") int id) {
+        service.delete(id);
+        return "redirect:/";
+    }
+
+}
---
+++ src/main/java/com/example/EmployeeCRUD/domain/Employee.java
@@ -0,0 +1,51 @@
+package com.example.EmployeeCRUD.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	@Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    public long id;
+    public String name;
+    public int age;
+    public int salary;
+	public Employee() {
+ 
+	}
+	public Employee(long id, String name, int age, int salary) {
+	
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+}
