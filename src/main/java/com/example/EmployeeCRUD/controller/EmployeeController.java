8813358c616d915a57e9be96ed0c7adad08c7be8
A Alyque Hazin <alyquekj@gmail.com>
2021-06-24 23:45:35 +0530

    modified app.java

--- src/main/java/com/example/EmployeeCRUD/controller/EmployeeController.java
+++ src/main/java/com/example/EmployeeCRUD/controller/EmployeeController.java
@@ -8,6 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +38,7 @@ public class EmployeeController {
         return "new";
     }
 
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @RequestMapping(value = "/save",method = RequestMethod.POST)
     public String saveStudent(@ModelAttribute("employee") Employee std) {
         service.save(std);
         return "redirect:/";
